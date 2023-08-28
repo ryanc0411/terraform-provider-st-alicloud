8dee02cbc4c3486add5e257fbbb4da0ca58710cc
Xiao Tong <66934914+WongXiaoTong@users.noreply.github.com>
2023-08-28 13:34:36 +0800

    Bugfix: The combined policies attached to the user may be deleted due to human mistake or API error. (#29)
    Co-authored-by: XIAOTONG WONG

--- alicloud/resource_ram_policy.go
+++ alicloud/resource_ram_policy.go
@@ -183,6 +183,19 @@ func (r *ramPolicyResource) Read(ctx context.Context, req resource.ReadRequest, 
 		return
 	}
 
+	// This state will be using to compare with the current state.
+	var oriState *ramPolicyResourceModel
+	getOriStateDiags := req.State.Get(ctx, &oriState)
+	resp.Diagnostics.Append(getOriStateDiags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if len(state.Policies.Elements()) != len(oriState.Policies.Elements()) {
+		resp.Diagnostics.AddWarning("Combined policies not found.", "The combined policies attached to the user may be deleted due to human mistake or API error.")
+		state.AttachedPolicies = types.ListNull(types.StringType)
+	}
+
 	setStateDiags := resp.State.Set(ctx, &state)
 	resp.Diagnostics.Append(setStateDiags...)
 	if resp.Diagnostics.HasError() {
@@ -418,12 +431,15 @@ func (r *ramPolicyResource) readPolicy(state *ramPolicyResourceModel) diag.Diagn
 				handleAPIError(err)
 			}
 
-			if getPolicyResponse.Body.Policy != nil {
-				policyDetail := policyDetail{
-					PolicyName:     types.StringValue(*getPolicyResponse.Body.Policy.PolicyName),
-					PolicyDocument: types.StringValue(*getPolicyResponse.Body.DefaultPolicyVersion.PolicyDocument),
+			// Sometimes combined policies may be removed accidentally by human mistake or API error.
+			if getPolicyResponse.Body != nil && getPolicyResponse.Body.Policy != nil {
+				if getPolicyResponse.Body.Policy.PolicyName != nil && getPolicyResponse.Body.DefaultPolicyVersion.PolicyDocument != nil {
+					policyDetail := policyDetail{
+						PolicyName:     types.StringValue(*getPolicyResponse.Body.Policy.PolicyName),
+						PolicyDocument: types.StringValue(*getPolicyResponse.Body.DefaultPolicyVersion.PolicyDocument),
+					}
+					policyDetailsState = append(policyDetailsState, &policyDetail)
 				}
-				policyDetailsState = append(policyDetailsState, &policyDetail)
 			}
 		}
 		return nil
@@ -441,30 +457,28 @@ func (r *ramPolicyResource) readPolicy(state *ramPolicyResourceModel) diag.Diagn
 		}
 	}
 
-	state = &ramPolicyResourceModel{}
+	policyDetails := []attr.Value{}
 	for _, policy := range policyDetailsState {
-		state.Policies = types.ListValueMust(
-			types.ObjectType{
-				AttrTypes: map[string]attr.Type{
-					"policy_name":     types.StringType,
-					"policy_document": types.StringType,
-				},
+		policyDetails = append(policyDetails, types.ObjectValueMust(
+			map[string]attr.Type{
+				"policy_name":     types.StringType,
+				"policy_document": types.StringType,
 			},
-			[]attr.Value{
-				types.ObjectValueMust(
-					map[string]attr.Type{
-						"policy_name":     types.StringType,
-						"policy_document": types.StringType,
-					},
-					map[string]attr.Value{
-						"policy_name":     types.StringValue(policy.PolicyName.ValueString()),
-						"policy_document": types.StringValue(policy.PolicyDocument.ValueString()),
-					},
-				),
+			map[string]attr.Value{
+				"policy_name":     types.StringValue(policy.PolicyName.ValueString()),
+				"policy_document": types.StringValue(policy.PolicyDocument.ValueString()),
 			},
-		)
+		))
 	}
-
+	state.Policies = types.ListValueMust(
+		types.ObjectType{
+			AttrTypes: map[string]attr.Type{
+				"policy_name":     types.StringType,
+				"policy_document": types.StringType,
+			},
+		},
+		policyDetails,
+	)
 	return nil
 }
 
